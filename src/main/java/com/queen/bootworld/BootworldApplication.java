aeedfc222b25d7fadc81da3bcdc2387c5471ed76
laven <lavender_wgt@163.com>
2024-03-29 21:16:07 +0800

    第一次提交

---
+++ src/main/java/com/queen/bootworld/BootworldApplication.java
@@ -0,0 +1,13 @@
+package com.queen.bootworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/queen/bootworld/BootworldApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.queen.bootworld;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BootworldApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
